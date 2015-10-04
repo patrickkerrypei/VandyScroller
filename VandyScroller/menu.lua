cf455fb7d760c8c0d6c3c1e7d99e1b101d2117dc
Ashwin Ignatius <aignatius3@gatech.edu>
2015-10-03 21:31:16 -0400

    Fixing the Title

--- VandyScroller/menu.lua
+++ VandyScroller/menu.lua
@@ -23,7 +23,7 @@ function scene:create( event )
 	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
 
 	-- display a background image
-	local background = display.newImageRect( "splash_screen.png", display.contentWidth, display.contentHeight )
+	local background = display.newImageRect( "MenuBackground.png", display.contentWidth, display.contentHeight )
 	background.anchorX = 0
 	background.anchorY = 0
 	background.x, background.y = 0, 0
